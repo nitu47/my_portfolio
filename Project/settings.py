9c3fad2dfeaa6d83991b150c77c6b2fcf762b12c
Nitu Kumari <nitu.kumari70386@gmail.com>
2025-11-22 15:06:52 +0545

    issues staticfiles..

--- Project/settings.py
+++ Project/settings.py
@@ -10,18 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
-import os
 from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-STATIC_URL = "/static/"
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-
-STATIC_ROOT = BASE_DIR / "staticfiles"
+import os
 
 
 
@@ -50,8 +40,8 @@ INSTALLED_APPS = [
     'my_portfolio',
 ]
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_URL = '/static_root/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_root')]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
